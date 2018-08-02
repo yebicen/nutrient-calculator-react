6a1f8cdac63985f6fe53cd01b3873a572bb54d71
Bicen Ye <yebicen@gmail.com>
2018-08-01 21:35:32 -0700

    initial

--- client/src/Mainrouter.js
+++ client/src/Mainrouter.js
@@ -3,15 +3,22 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Ingredients from "./components/Ingredients";
 import { Container } from 'reactstrap';
-
+import NavBar from "./components/NavBar";
+import JumbotronComponent from "./components/Jumbotron";
 
 const App = () =>
     <Container>
         <Router>
             <div>
+                <JumbotronComponent/>
+                <NavBar />
                 <Route path="/Ingredients" component={Ingredients} />
             </div>
         </Router>
     </Container>
+
+    
+
+    
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Mainrouter from "./Mainrouter.js"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Mainrouter />, document.getElementById("root"));
 
