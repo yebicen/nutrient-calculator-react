d1bf944704231ff65294ee94acbbfc793dd72f29
Levan <l.dvalidze@gmail.com>
2018-08-04 10:35:43 -0700

    main router updated

--- client/src/Mainrouter.js
+++ client/src/Mainrouter.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Ingredients from "./components/Ingredients";
+import Recipes from "./components/Recipes";
 import { Container } from 'reactstrap';
 import NavBar from "./components/NavBar";
 import JumbotronComponent from "./components/Jumbotron";
@@ -16,6 +17,7 @@ const App = () =>
                 <NavBar />
                 <Route exact path="/" component={Smoothie} />
                 <Route exact path="/Ingredients" component={Ingredients} />
+                <Route exact path="/Recipes" component={Recipes} />
                 {/* <Route path="/Ingredients" component={Recipes} /> */}
             </div>
         </Router>
