8003bea88d72b88870ec4b42516ea74033bd15b5
Levan <l.dvalidze@gmail.com>
2018-08-07 13:16:10 -0700

    RecipeList added

--- client/src/components/Recipes/RecipeList.js
+++ client/src/components/Recipes/RecipeList.js
@@ -1,104 +1,49 @@
 import React from 'react';
-import { Table, Button } from 'reactstrap';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFlag } from '@fortawesome/free-solid-svg-icons';
-import DeleteModal from './DeleteModal';
-import EditModal from './EditModal';
-
+import { Container, Table } from 'reactstrap';
 import API from '../../utils/API';
 
-library.add(faFlag)
 
-export default class IngredientList extends React.Component {
-
+export default class RecipeContainer extends React.Component {
     state = {
-        //for ingredient deletion
-        deleteModal: false,
-        deleteIngredientName: "",
-        deleteIngredientId: "",
-
-        //for ingredient editing
-        editModal: false,
-        editIngredientName: "",
-        editIngredientId: "",
-
-        prepopulateForm: {}
+        dbIngredients: [],
+        dbRecipes: [],
+        dbRecipeAmounts: []
     }
 
-    toggleDeleteModal = (deleteIngredientName, deleteIngredientId) => {
-        this.setState({
-            deleteModal: !this.state.deleteModal,
-            deleteIngredientName: deleteIngredientName,
-            deleteIngredientId: deleteIngredientId
-        });
-    }
-
-    toggleEditModal = (editIngredientName, editIngredientId) => {
-        API.getOneIngredient(editIngredientId)
+    getRecipes = () => {
+        API.getRecipes()
             .then(res => {
                 this.setState({
-                    editModal: !this.state.editModal,
-                    editIngredientName: editIngredientName,
-                    editIngredientId: editIngredientId,
-                    prepopulateForm: res.data
+                    dbIngredients: res.data[0],
+                    dbRecipes: res.data[1],
+                    dbRecipeAmounts: res.data[2]
                 })
-
-                this.state.editModal==true ? this.child.setEditState() : null;
             })
+            
     }
+
+    componentDidMount() {
+        this.getRecipes();
+    };
 
     render() {
         return (
-            <div className="ingredientListWrap">
+            
                 <Table striped className="ingredient list">
                     <thead>
                         <tr>
-                            <th>Ingredient name</th>
-                            <th className="ingredient-values">Calories</th>
-                            <th className="ingredient-values">Sugar</th>
-                            <th className="ingredient-values">Fat</th>
-                            <th className="ingredient-values">Protein</th>
-                            <th className="ingredient-values">Carbs</th>
-                            <th className="ingredient-values">Has Gluten?</th>
-                            <th className="ingredient-values">Is a Nut?</th>
-                            <th className="ingredient-values">Edit</th>
-                            <th className="ingredient-values">Delete</th>
+                            <th>List of Recipes </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.dbIngredients.map(item => (
+                        {this.state.dbRecipes.map(item => (
                             <tr key={item.id}>
-                                <td>{item.IngredientName}</td>
-                                <td className="ingredient-values">{item.Calories}</td>
-                                <td className="ingredient-values">{item.Sugar}</td>
-                                <td className="ingredient-values">{item.Fat}</td>
-                                <td className="ingredient-values">{item.Protein}</td>
-                                <td className="ingredient-values">{item.Carbs}</td>
-                                <td className="ingredient-values flag">{item.hasGluten ? <FontAwesomeIcon icon="flag" /> : null}</td>
-                                <td className="ingredient-values flag">{item.isNut ? <FontAwesomeIcon icon="flag" /> : null}</td>
-                                <td className="ingredient-values"><Button color="primary" onClick={() => this.toggleEditModal(item.IngredientName, item.id)}>Edit</Button></td>
-                                <td className="ingredient-values"><Button color="danger" onClick={() => this.toggleDeleteModal(item.IngredientName, item.id)}>Delete</Button></td>
+                                <td>{item.RecipeName}</td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
-                <DeleteModal
-                    toggle={this.toggleDeleteModal}
-                    modal={this.state.deleteModal}
-                    deleteIngredientName={this.state.deleteIngredientName}
-                    deleteIngredientId={this.state.deleteIngredientId}
-                    getIngredients={this.props.getIngredients}
-                />
-                <EditModal ref={instance => { this.child = instance; }}
-                    toggle={this.toggleEditModal}
-                    modal={this.state.editModal}
-                    editIngredientName={this.state.editIngredientName}
-                    editIngredientId={this.state.editIngredientId}
-                    prepopulateForm={this.state.prepopulateForm}
-                    getIngredients={this.props.getIngredients}
-                />
-            </div>
-        )
+            
+        );
     }
 }
