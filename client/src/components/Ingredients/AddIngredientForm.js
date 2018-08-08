89004712f8e0cfbfa28d7b219f921b391b97772d
Levan <l.dvalidze@gmail.com>
2018-08-08 16:44:47 -0700

    ingredient form validations working

--- client/src/components/Ingredients/AddIngredientForm.js
+++ client/src/components/Ingredients/AddIngredientForm.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
 import API from '../../utils/API';
-import IngredientForm from './IngredientForm'
+// import IngredientForm from './IngredientForm'
+import IngredientForm from './Valid_IngredientForm.js'
 
 export default class AddIngredientForm extends React.Component {
   state = {
     IngredientName: "",
-    Calories: "",
-    Carbs: "",
-    Sugar: "",
-    Fat: "",
-    Protein: "",
+    Calories: 11,
+    Carbs: 11,
+    Sugar: 11,
+    Fat: 11,
+    Protein: 11,
     hasGluten: false,
     isNut: false,
     isGMO: false
@@ -29,7 +31,7 @@ export default class AddIngredientForm extends React.Component {
   handleFormSubmit = event => {
     event.preventDefault();
     API.addIngredient(this.state).then(
-        this.props.getIngredients()
+      this.props.getIngredients()
     )
     this.setState({
       IngredientName: "",
@@ -44,15 +46,18 @@ export default class AddIngredientForm extends React.Component {
     });
   };
 
-  render () {
+  render() {
     return (
       <div className="addIngredientForm">
-      <IngredientForm 
-        handleInputChange={this.handleInputChange} 
-        state={this.state}
-      />
-      <Button onClick={this.handleFormSubmit}>Submit</Button>
+        <AvForm>
+          <IngredientForm
+            handleInputChange={this.handleInputChange}
+            state={this.state}
+          />
+
+          <Button onClick={this.handleFormSubmit}>Submit</Button>
+        </AvForm>
       </div>
-      )
+    )
   }
 }
---
+++ client/src/components/Ingredients/Valid_IngredientForm.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { AvForm, AvField, AvGroup, AvInput } from 'availity-reactstrap-validation';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+export default class IngredientForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+
+            <div>
+                <AvField type="text" name="IngredientName" label="IngredientName" value={this.props.IngredientName} onChange={this.props.handleInputChange} required />
+                <AvField type="number" name="Calories" label="Calories per oz" value={this.props.Calories} onChange={this.props.handleInputChange} required />
+                <AvField type="number" name="Carbs" label="Carbs per oz" value={this.props.Carbs} onChange={this.props.handleInputChange} required />
+                <AvField type="number" name="Sugar" label="Sugar per oz" value={this.props.Sugar} onChange={this.props.handleInputChange} required />
+                <AvField type="number" name="Fat" label="Fat per oz" value={this.props.Fat} onChange={this.props.handleInputChange} required />
+                <AvField type="number" name="Protein" label="Protein per oz" value={this.props.Protein} onChange={this.props.handleInputChange} required />
+                <AvGroup check>
+                    <Label check>
+                        <AvInput type="checkbox" name="hasGluten" checked={this.props.hasGluten} onChange={this.props.handleInputChange} /> Has Gluten?
+                    </Label>
+                </AvGroup>
+                <AvGroup check>
+                    <Label check>
+                        <AvInput type="checkbox" name="isNut" checked={this.props.isNut} onChange={this.props.handleInputChange} /> Is Nut?
+                    </Label>
+                </AvGroup>
+                <AvGroup check>
+                    <Label check>
+                        <AvInput type="checkbox" name="isGMO" checked={this.props.isGMO} onChange={this.props.handleInputChange} /> Is GMO?
+                    </Label>
+                </AvGroup>
+            </div>
+            // <Form>
+            //   <FormGroup>
+            //     <Label for="IngredientName">Ingredient Name</Label>
+            //     <Input type="text" name="IngredientName" value={IngredientName} onChange={handleInputChange} placeholder="Ingredient Name"/>
+            //   </FormGroup>
+            //   <FormGroup>
+            //     <Label for="Calories">Calories per oz</Label>
+            //     <Input type="number" name="Calories" value={Calories} onChange={handleInputChange} placeholder="Calories per oz"/>
+            //   </FormGroup>
+            //   <FormGroup>
+            //     <Label for="Carbs">Carbs per oz</Label>
+            //     <Input type="number" name="Carbs" value={Carbs} onChange={handleInputChange} placeholder="Carbs per oz"/>
+            //   </FormGroup>
+            //   <FormGroup>
+            //     <Label for="Sugar">Sugar per oz</Label>
+            //     <Input type="number" name="Sugar" value={Sugar} onChange={handleInputChange} placeholder="Sugar per oz"/>
+            //   </FormGroup>
+            //   <FormGroup>
+            //     <Label for="Fat">Fat per oz</Label>
+            //     <Input type="number" name="Fat" value={Fat} onChange={handleInputChange} placeholder="Fat per oz"/>
+            //   </FormGroup>
+            //   <FormGroup>
+            //     <Label for="Protein">Protein per oz</Label>
+            //     <Input type="number" name="Protein" value={Protein} onChange={handleInputChange} placeholder="Protein per oz"/>
+            //   </FormGroup>
+            //   <FormGroup check>
+            //     <Label check>
+            //       <Input type="checkbox" name="hasGluten" checked={hasGluten} onChange={handleInputChange}/>
+            //       Has Gluten?
+            //               </Label>
+            //   </FormGroup>
+            //   <FormGroup check>
+            //     <Label check>
+            //       <Input type="checkbox" name="isNut" checked={isNut} onChange={handleInputChange} />
+            //       Is a Nut?
+            //               </Label>
+            //   </FormGroup>
+            //   <FormGroup check>
+            //     <Label check>
+            //       <Input type="checkbox" name="isGMO" checked={isGMO} onChange={handleInputChange} />
+            //       is GMO?
+            //               </Label>
+            //   </FormGroup>
+            // </Form>
+        )
+    }
+}
