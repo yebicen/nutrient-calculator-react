9c1218a7da6b04e350a77df4af28c09125dd44f6
Levan <l.dvalidze@gmail.com>
2018-08-09 21:01:54 -0700

    fixed the bug

--- client/src/Mainrouter.js
+++ client/src/Mainrouter.js
@@ -10,6 +10,7 @@ import JumbotronComponent from "./components/Jumbotron";
 import Smoothie from "./components/Smoothie";
 import Login from "./components/login";
 import Profile from "./components/profile";
+import Imguploader from "./components/uploadimg";
  
 const App = () =>
     <Container> 
