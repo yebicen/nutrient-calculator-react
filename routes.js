e58f583dd3635be45a42a88c4319683b94f9b900
George Chanturidze <george.chanturidze@gmail.com>
2018-08-10 19:11:12 -0700

    renamed routes with /api

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports = function(app){
     const users = require('./routes/users');
     const recipes_routes = require('./routes/Recipes_routes');
 
-    app.use('/recipes', recipes_routes);
+    app.use('/api/recipes', recipes_routes);
     app.use('/users',users);
-    app.use('/ingredients', ingredients_routes);
+    app.use('/api/ingredients', ingredients_routes);
 }
