e4988d2ca11ee3bdbf23f19cdf266eb58a498ba9
George Chanturidze <george.chanturidze@gmail.com>
2018-08-15 14:57:25 -0700

    try changes in server.js

--- server.js
+++ server.js
@@ -72,16 +72,19 @@ require('./routes')(app);
 // Serve static content for the app from the "public" directory in the application directory.
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 
 else {
   app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
-app.get("*", function(req, res) {
-  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+
 
 // db.sequelize.sync({force: true}).then(function() {
   db.sequelize.sync().then(function() {
