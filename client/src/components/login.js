9917f4f6f503aecf3fcfb9e6ecf4bd7d537787ce
George Chanturidze <george.chanturidze@gmail.com>
2018-08-16 13:44:31 -0700

    login fix

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,70 +1,83 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
 import UserAPI from "../utils/UserAPI";
 import history from '../history';
 
 console.log(history.location);
 export default class Login extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
-        this.state = {         
-          usernameLgIn: "",
-          passwordLgIn: ""
+        this.state = {
+            usernameLgIn: "",
+            passwordLgIn: ""
         };
         this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
         this.handleLoginChange = this.handleLoginChange.bind(this);
-      }
+    }
 
 
-     handleLoginChange = event => {
-         this.setState({
-             [event.target.name]: event.target.value
-         })
-         console.log(this.state);
-     }
+    handleLoginChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state);
+    }
 
-     handleLoginSubmit = event => {
-         event.preventDefault();
-         const login = {
-             username: this.state.usernameLgIn,
-             password: this.state.passwordLgIn
-         }
-         UserAPI.loginUser(login)
-        .then(res => {
-          console.log("logged In");
-         })
-        .catch(err => console.log(err));
-        history.push('/');
-         this.setState({
-             usernameLgIn: "",
-             passwordLgIn: ""
-         })
-      window.location.reload(); 
-         
-     }
-     
-     render() {
+    handleLoginSubmit = event => {
+        event.preventDefault();
+        const login = {
+            username: this.state.usernameLgIn,
+            password: this.state.passwordLgIn
+        }
+        UserAPI.loginUser(login)
+            .then(res => {
+                console.log("logged In");
+            })
+            .catch(err => console.log(err));
+        // history.push('/');
+        this.setState({
+            usernameLgIn: "",
+            passwordLgIn: ""
+        })
+        window.location.reload();
 
-       return (
-           <div>
-              <Form inline>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                   <label for="usernameLgIn" className="mr-sm-2">Userame: </label>                
-                   <Input id="usernameLgIn" name = "usernameLgIn" value = {this.state.usernameLgIn} onChange = {this.handleLoginChange} />                
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                   <label for="passwordLgIn" className="mr-sm-2">Password: </label>               
-                   <Input id="passwordLgIn" name = "passwordLgIn" value = {this.state.passwordLgIn} onChange = {this.handleLoginChange} />               
-                </FormGroup>
-                <Button onClick = {this.handleLoginSubmit} >Submit </Button>
+    }
 
-              </Form>
-           </div>
+    render() {
 
-       );
+        return (
+            
+            <Container>
+                
+                <h4>Log in to manage ingredients and recipes</h4>
+                <Form>
+                    <Row className="loginForm">
+                        <Col md="3">
+                            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                <label for="usernameLgIn" className="mr-sm-2">Userame: </label>
+                                <Input id="usernameLgIn" name="usernameLgIn" value={this.state.usernameLgIn} onChange={this.handleLoginChange} />
+                            </FormGroup>
+                        </Col>
+                        <Col md="3">
+                            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                <label for="passwordLgIn" className="mr-sm-2">Password: </label>
+                                <Input id="passwordLgIn" name="passwordLgIn" value={this.state.passwordLgIn} onChange={this.handleLoginChange} />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row className="loginForm">
+                        <Col>
+                        <Button color="primary" onClick={this.handleLoginSubmit} >Submit </Button>
+                        </Col>
+                    </Row>
+                </Form>
+
+            </Container>
+
+        );
 
 
-     }
-    
+    }
+
 
 }
